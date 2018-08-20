f9f12c13f02fdd76888938ef57e2111a2f92a7d4
Michal vel m@lbit <michal@arqma.com>
2018-08-21 00:50:32 +0100

    Update build-all-arch.sh

--- build-all-arch.sh
+++ build-all-arch.sh
@@ -38,9 +38,9 @@ base_dir=`pwd`
  	mkdir -p $OUTPUT_DIR
 	cd $OUTPUT_DIR
  	PATH=/opt/android/tool/$arch/$target_host/bin:/opt/android/tool/$arch/bin:$PATH CC=clang CXX=clang++ cmake -D BUILD_GUI_DEPS=1 -D BUILD_TESTS=OFF -D ARCH="$xarch" -D STATIC=ON -D BUILD_64=$sixtyfour -D CMAKE_BUILD_TYPE=$build_type -D ANDROID=true -D BUILD_TAG="android" -D BOOST_ROOT=/opt/android/build/boost/$arch -D BOOST_LIBRARYDIR=/opt/android/build/boost/$arch/lib -D OPENSSL_INCLUDE_DIR=/opt/android/build/openssl/include OPENSSL_ROOT_DIR=/opt/android/build/openssl/$arch -D OPENSSL_CRYPTO_LIBRARY=/opt/android/build/openssl/$arch/lib/libcrypto.so -D OPENSSL_SSL_LIBRARY=/opt/android/build/openssl/$arch/lib/libssl.so -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=true ../..
-	CMAKE_INCLUDE_PATH=/opt/android/cppzmq/
-	CMAKE_LIBRARY_PATH=/opt/android/$arch/zeromq4-1/.libs
-	CXXFLAGS="-I /opt/android/$arch/zeromq4-1/include/"
+	#CMAKE_INCLUDE_PATH=/opt/android/cppzmq/
+	#CMAKE_LIBRARY_PATH=/opt/android/$arch/zeromq4-1/.libs
+	#CXXFLAGS="-I /opt/android/$arch/zeromq4-1/include/"
 	make -j2 wallet_api
 	find . -path ./lib -prune -o -name '*.a' -exec cp '{}' lib \;
      TARGET_LIB_DIR=/opt/android/build/arqma/$arch/lib
