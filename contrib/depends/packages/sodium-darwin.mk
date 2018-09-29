1fe201872d168560b66ae6c5cdd4dddcb3858010
Michal vel m@lbit <michal@arqma.com>
2018-09-29 13:35:04 +0100

    fixes
    + build: set ARCH_FLAG before compiler/linker flag checks
    + Use local SDK tools, when compiling apple depends
    + OSX compilation fix: missing boost lib added

--- contrib/depends/packages/sodium-darwin.mk
+++ contrib/depends/packages/sodium-darwin.mk
@@ -6,12 +6,13 @@ $(package)_sha256_hash=fb6a9e879a2f674592e4328c5d9f79f082405ee4bb05cb6e679b90afe
 
 define $(package)_set_vars
 $(package)_config_opts=--enable-static
+$(package)_build_opts_darwin=OS=Darwin LIBTOOL="$($(package)_libtool)"
 $(package)_config_opts+=--prefix=$(host_prefix)
 endef
 
 define $(package)_config_cmds
   ./autogen.sh &&\
-  $($(package)_autoconf) $($(package)_config_opts) RANLIB="$(host_prefix)/native/bin/x86_64-apple-darwin11-ranlib" AR="$(host_prefix)/native/bin/x86_64-apple-darwin11-ar"
+	$($(package)_autoconf) $($(package)_config_opts) RANLIB="$(host_prefix)/native/bin/x86_64-apple-darwin11-ranlib" AR="$(host_prefix)/native/bin/x86_64-apple-darwin11-ar" CC="$(host_prefix)/native/bin/$($(package)_cc)"
 endef
 
 define $(package)_build_cmds
